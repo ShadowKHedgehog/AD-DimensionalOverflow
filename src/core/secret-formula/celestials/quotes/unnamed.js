d07db1924e3e573500ca015424c1823ad11de256
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2025-09-11 15:19:00 -0400

    Create unnamed.js

---
+++ src/core/secret-formula/celestials/quotes/unnamed.js
@@ -0,0 +1,62 @@
+export const unnamedQuotes = {
+  initial: {
+    id: 0,
+    lines: [
+      "....",
+      { text: "Suffering.. Thats all thats there..", showCelestialName: false }
+      { text: "I will pass to live another day..", showCelestialName: false }
+      { text: "Wont you call out your friends again?", showCelestialName: false }
+      { text: "Maybe we may meet soon enough.", showCelestialName: false }
+      { text: "See you soon, Destroyer.", showCelestialName: false }
+    ]
+  },
+  unlockcel: {
+    id: 1,
+    lines: [
+      "Hmm.... Who are you?.",
+      "I see you've bested Pelle.",
+      "The others... They are completely fine.",
+      "Time will pass, the inevitable looming comes",
+      "Maybe you can help me Remember who I am.",
+      "Hopefully, I remember everything..",
+    ]
+  },
+  unlockRun: {
+    id: 2,
+    lines: [
+      "I see... I have a 'reality' as you call it.. Huh.",
+      "I will let you challenge my Reality.",
+      { text: "You have unlocked Unnamed's Reality.", showCelestialName: false }
+    ]
+  },
+  startRun: {
+    id: 3,
+    lines: [
+      ".",
+      "They squeezed... this Reality... too tightly. Cracks appeared.",
+      "Search... everywhere. We will help... where we can.",
+    ]
+  },
+  UnlockPermGalaxyGen: {
+    id: 4,
+    lines: [
+      "Thank you for helping me recover some of my memories.",
+      "I have you to thank. I now grant you access to MY Galaxy Generator",
+      { text: "You have unlocked the Permanent Galaxy Generator.", showCelestialName: false }
+    ]
+  },
+  runec12ic8: {
+    id: 5,
+    lines: [
+      "... did not... underestimate you..."
+    ]
+  },
+  completeReality: {
+    id: 6,
+    lines: [
+      "All... fragments... clones... freed.",
+      "We have given... tools... of our imprisoning. Use them...",
+      "Freedom from torture... is torture itself.",
+    ]
+  },
+};
