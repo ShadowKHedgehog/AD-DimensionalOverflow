787538a1db69c236cae0c94ff03846c1822c9dc2
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2025-09-16 14:04:29 -0400

    da
    da

--- src/core/dimensions/antimatter-dimension.js
+++ src/core/dimensions/antimatter-dimension.js
@@ -328,7 +328,7 @@ export function buyMaxDimension(tier, bulk = Infinity) {
     return;
   }
   let buying = maxBought.quantity;
-  if (buying.gt(bulkLeft)) buying = new Decimal(bulkLeft).times(100);
+  if (buying.gt(bulkLeft)) buying = new Decimal(bulkLeft);
   dimension.amount = dimension.amount.plus(buying);
   dimension.bought = dimension.bought.add(buying);
   dimension.currencyAmount = dimension.currencyAmount.minus(Decimal.pow10(maxBought.logPrice)).max(0);
