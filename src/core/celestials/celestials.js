8c47014c3a18d5d66ad2ec51f6249dbb550ad1d4
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2025-11-13 13:11:45 -0500

    da
    da

--- src/core/celestials/celestials.js
+++ src/core/celestials/celestials.js
@@ -5,6 +5,7 @@ import { Pelle } from "./pelle/pelle";
 import { Ra } from "./ra/ra";
 import { Teresa } from "./teresa";
 import { V } from "./V";
+import { Sercanote } from "./Sercanote/Sercanote";
 
 export const Celestials = {
   teresa: Teresa,
@@ -13,7 +14,8 @@ export const Celestials = {
   v: V,
   ra: Ra,
   laitela: Laitela,
-  pelle: Pelle
+  pelle: Pelle,
+  sercanote: Sercanote,
 };
 
 GameDatabase.celestials.descriptions = [
