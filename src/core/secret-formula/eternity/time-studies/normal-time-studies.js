22247f390f6b7dd40bbe5e2b70e66c0ae5e651f8
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2026-06-03 12:59:43 -0400

    da

--- src/core/secret-formula/eternity/time-studies/normal-time-studies.js
+++ src/core/secret-formula/eternity/time-studies/normal-time-studies.js
@@ -456,7 +456,7 @@ export const normalTimeStudies = [
   {
     id: 193,
     cost: new Decimal(300),
-    requirement: [181, () => EternityChallenge(10).completions > 0 || Perk.BypassEC10Lock.isBought],
+    requirement: [181, () => EternityChallenge(10).completions > 0 || Perk.bypassEC10Lock.isBought],
     reqType: TS_REQUIREMENT_TYPE.ALL,
     description: "Antimatter Dimension multiplier based on Eternities",
     effect: () => (DC.E13000.pow(Currency.eternities.value.div(1e6).clampMax(1))),
