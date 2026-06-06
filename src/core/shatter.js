47b3468731beef1f69bd04c0acd446dd42aff1e7
Person885 <keyboaqwerty@gmail.com>
2026-06-06 13:30:08 -0600

    Update shatter.js

--- src/core/shatter.js
+++ src/core/shatter.js
@@ -1,22 +1,22 @@
-export function shatterResetRequest {
+export function shatterResetRequest() {
   if (!Player.canShatter) return;
   shatterReset();
 }
-export function shatterReset {
+export function shatterReset() {
   if (!Player.canShatter) return;
   EventHub.dispatch(GAME_EVENT.SHATTER_RESET_BEFORE);
   shatterGiveRewards();
   restartWithCarryoverPostShatter();
   EventHub.dispatch(GAME_EVENT.SHATTER_RESET_AFTER);
 }
-function shatterGiveRewards {
+function shatterGiveRewards() {
   shatterUpdateStatistics();
 
   const shatterShards = gainedShatterShards();
   Currency.shattershards.add(shatterShards);
   player.exposes = player.exposes.add(gainedExposes());
 }
-function shatterUpdateStatistics {
+function shatterUpdateStatistics() {
   player.records.bestShatter.time = Decimal.min(player.records.thisShatter.time, player.records.bestShatter.time);
   player.records.bestShatter.trueTime = Decimal.min(player.records.thisShatter.trueTime, player.records.bestShatter.time).toNumber();
   player.records.bestShatter.realTime = Decimal.min(player.records.thisShatter.realTime, player.records.bestShatter.time);
