196b1cf661bf43011d462231cefd42740ead4600
Supersonic Seven <b.rockholz@icloud.com>
2026-06-19 17:32:04 -0400

    Add ShatterButton component with functionality

---
+++ src/components/ui-modes/prestige-header/ShatterButton.vue
@@ -0,0 +1,64 @@
+<script>
+export default {
+  name: "ShatterButton",
+  data() {
+    return {
+      canShatter: false,
+      gainedSS: 0
+    };
+  },
+  computed: {
+    formatSSGain() {
+      return `Shattershards gained: ${format(this.gainedSS, 2)}`;
+    },
+    classObject() {
+      return {
+        "c-shatter-button--unlocked": this.canShatter,
+        "c-shatter-button--locked": !this.canShatter
+      };
+    }
+  },
+  methods: {
+    update() {
+      this.canShatter = isShatterAvailable();
+      this.showPelleGlow = true;
+      if (!this.canShatter) {
+        this.gainedSS = 0;
+        return;
+      }
+      this.gainedSS = gainedShattershards();
+    },
+    handleClick() {
+      if (this.canShatter) {
+        shatter();
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <div class="l-shatter-button">
+    <button
+      class="c-shatter-button infotooltip"
+      :class="classObject"
+      @click="handleClick"
+    >
+      <div class="l-shatter-button__contents">
+        <template v-if="canShatter">
+          <div class="c-shatter-button__header">
+            Shatter
+          </div>
+          <div>{{ formatSSGain }}</div>
+        </template>
+        <template v-else>
+          <div>Reach {{ format("1e9000000000000000") }} Antimatter to unlock the ability to Shatter</div>
+        </template>
+      </div>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
