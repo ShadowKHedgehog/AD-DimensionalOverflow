a363c96bb49a7d4da6c9323e9f6e536e17dcf1fd
Supersonic Seven <b.rockholz@icloud.com>
2026-06-19 17:38:16 -0400

    Add ShatterButton to HeaderCenterContainer component

--- src/components/ui-modes/prestige-header/HeaderCenterContainer.vue
+++ src/components/ui-modes/prestige-header/HeaderCenterContainer.vue
@@ -6,6 +6,8 @@ import HeaderTickspeedInfo from "../HeaderTickspeedInfo";
 
 import RealityButton from "./RealityButton";
 
+import ShatterButton from "./ShatterButton";
+
 // This component contains antimatter and antimatter rate at the start of the game, as well as some additional
 // information depending on the UI (tickspeed for Classic, game speed for Modern). Everything but antimatter is
 // removed once Reality is unlocked, to make room for the reality button
@@ -16,6 +18,7 @@ export default {
     RealityCurrencyHeader,
     RealityButton,
     ArmageddonButton,
+    ShatterButton,
   },
   data() {
     return {
@@ -25,6 +28,9 @@ export default {
       isDoomed: false,
       antimatter: new Decimal(0),
       antimatterPerSec: new Decimal(0),
+      shattershards: new Decimal(0),
+      showShatter: false,
+      canShatter: false
     };
   },
   methods: {
@@ -37,6 +43,9 @@ export default {
       this.antimatter.copyFrom(Currency.antimatter);
       this.hasRealityButton = PlayerProgress.realityUnlocked() || TimeStudy.reality.isBought;
       if (!this.hasRealityButton) this.antimatterPerSec.copyFrom(Currency.antimatter.productionPerSecond);
+      this.shattershards.copyFrom(Currency.shattershards);
+      this.showShatter = PlayerProgress.shatterUnlocked();
+      this.canShatter = player.antimatter.gte("e9e15") && Pelle.isDoomed;
     },
   },
 };
@@ -47,6 +56,11 @@ export default {
     v-if="shouldDisplay"
     class="c-prestige-button-container"
   >
+    <div
+      v-if="showEndgame"
+    >
+      You have <span class="ss-text">{{ format(shattershards, 2) }}</span> {{ pluralize("Shattershards", shattershards) }}.
+    <br>
     <span>You have <span class="c-game-header__antimatter">{{ format(antimatter, 2, 1) }}</span> antimatter.</span>
     <div
       v-if="hasRealityButton"
@@ -54,7 +68,11 @@ export default {
     >
       <RealityCurrencyHeader />
       <ArmageddonButton
-        v-if="isDoomed"
+        v-if="isDoomed && (!canShatter || !showShatter)"
+        :is-header="true"
+      />
+      <ShatterButton
+        v-if="showShatter && canShatter"
         :is-header="true"
       />
       <RealityButton v-else />
@@ -73,5 +91,8 @@ export default {
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+}
+.ss-text {
+  color: var(--color-pelle--base);
 }
 </style>
