0c745b1c623c835bf8dd8af279addd2344848f24
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2026-06-29 22:26:56 -0400

    da

--- src/core/new-game.js
+++ src/core/new-game.js
@@ -492,7 +492,7 @@ restartWithCarryoverPostShatter() {
     let rowProtect = 0;
     rowProtect = player.reality.glyphs.protectedRows;
     GameEnd = false;
-    GameEnd.removeAdditionalEnd = true;
+    GameEnd.removeAdditionalEnd = 15;
     player.isGameEnd = false;
     player.isGameEnd2 = false;
     Pelle.isGameEnd = false;
