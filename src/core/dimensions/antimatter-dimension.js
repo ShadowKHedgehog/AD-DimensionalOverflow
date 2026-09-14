4592a5c746ab52c69f84c92dd1930efd9d97e9ec
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2026-09-14 11:51:30 -0400

    Update antimatter-dimension.js

--- src/core/dimensions/antimatter-dimension.js
+++ src/core/dimensions/antimatter-dimension.js
@@ -641,9 +641,6 @@ class AntimatterDimensionState extends DimensionState {
         production = DC.D0;
       }
     }
-    if (Pelle.isDoomed && player.Exposes.lt(1) && production.gte(DC.BIMAX)) {
-      production = DC.D0 
-    }
     return production;
   }
 }
