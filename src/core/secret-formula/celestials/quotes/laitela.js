3bbc4152d0fab84abddef91cbb7eaebccc97e184
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2026-09-19 21:20:09 -0400

    da

--- src/core/secret-formula/celestials/quotes/laitela.js
+++ src/core/secret-formula/celestials/quotes/laitela.js
@@ -136,7 +136,7 @@ export const laitelaQuotes = {
   },
   darkmatterHardcapRaise: {
     id: 10,
-    requirement: () => Currency.darkmatter.gte(new Decimal("1.9e308")),
+    requirement: () => player.celestials.laitela.darkmatter.gte(new Decimal("1.9e308")),
     lines: [
       "What.... You.... No. No. NO! This shouldnt be possible! Just.. what are you, Destroyer?",
       "You are breaking everything I know.",
