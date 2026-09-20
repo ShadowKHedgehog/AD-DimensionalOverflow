7b8525b0b9555c1c1361c58d4be83bbd7113d020
ShadowTheHedgehog <Christianthehedgehogtessit@gmail.com>
2026-09-19 21:08:29 -0400

    da

--- src/core/celestials/pelle/galaxy-generator.js
+++ src/core/celestials/pelle/galaxy-generator.js
@@ -92,54 +92,77 @@ export const GalaxyGenerator = {
   },
 
   loop(diff) {
-    if (this.isCapped) {
-      Pelle.quotes.galaxyGeneratorRifts.show();
-    }
-    if (this.sacrificeActive) {
-      this.capRift.reducedTo = Decimal.max(Decimal.sub(this.capRift.reducedTo, (diff.div(1e5).mul(3))), 0).toNumber();
-      if (this.capRift.reducedTo === 0) {
-        player.celestials.pelle.galaxyGenerator.sacrificeActive = false;
-        player.celestials.pelle.galaxyGenerator.phase++;
-
-        const phase = player.celestials.pelle.galaxyGenerator.phase;
-        if (phase === 1) {
-          Pelle.quotes.galaxyGeneratorPhase1.show();
-        } else if (phase === 4) {
-          Pelle.quotes.galaxyGeneratorPhase4.show();
-        }
-
-        if (!this.capObj) {
-          Pelle.quotes.end.show();
-        }
-      }
-      PelleRifts.all.forEach(x => x.checkMilestoneStates());
-
-      // Force-unequip glyphs when the player loses the respective milestone. We call the respec option as normally
-      // except for one particular case - when we want to respec into protected slots but have no room to do so. In
-      // that case, we force-respec into the inventory instead
-      if (!PelleRifts.vacuum.milestones[0].canBeApplied && Glyphs.active.filter(g => g).length > 0) {
-        Glyphs.unequipAll(player.options.respecIntoProtected && Glyphs.findFreeIndex(true) === -1);
-        Glyphs.refreshActive();
-      }
-
-    }
-    player.celestials.pelle.galaxyGenerator.generatedGalaxies =
-      player.celestials.pelle.galaxyGenerator.generatedGalaxies.add(this.gainPerSecond.times(diff.div(1000)));
-    player.celestials.pelle.galaxyGenerator.generatedGalaxies = Decimal.min(
-      player.celestials.pelle.galaxyGenerator.generatedGalaxies,
-      this.generationCap
+  if (this.isCapped) {
+    Pelle.quotes.galaxyGeneratorRifts.show();
+  }
+  if (this.sacrificeActive) {
+    this.capRift.reducedTo = Decimal.max(
+      Decimal.sub(this.capRift.reducedTo, diff.div(1e5).mul(3)),
+      0
     );
+    if (this.capRift.reducedTo.eq(0)) {
+      player.celestials.pelle.galaxyGenerator.sacrificeActive = false;
+      player.celestials.pelle.galaxyGenerator.phase++;
 
-    if (!this.capRift) {
-    PelleRifts.all.forEach(r => 
-    r.reducedTo = diff.div(1e5).mul(3).add(r.reducedTo) .clampMax(Decimal.clampMax(player.exposes, 15).add(2)).toNumber()
-   ); if (PelleRifts.vacuum.milestones[0].canBeApplied && !this.hasReturnedGlyphSlot) {
-        Glyphs.refreshActive();
-        EventHub.dispatch(GAME_EVENT.GLYPHS_EQUIPPED_CHANGED);
-        this.hasReturnedGlyphSlot = true;
+      const phase = player.celestials.pelle.galaxyGenerator.phase;
+      if (phase === 1) {
+        Pelle.quotes.galaxyGeneratorPhase1.show();
+      } else if (phase === 4) {
+        Pelle.quotes.galaxyGeneratorPhase4.show();
       }
+
+      if (!this.capObj) {
+        Pelle.quotes.end.show();
+      }
+    }
+    PelleRifts.all.forEach(x => x.checkMilestoneStates());
+
+    // Force-unequip glyphs when the player loses the respective milestone. We call the respec option as normally
+    // except for one particular case - when we want to respec into protected slots but have no room to do so. In
+    // that case, we force-respec into the inventory instead
+    if (!PelleRifts.vacuum.milestones[0].canBeApplied && Glyphs.active.filter(g => g).length > 0) {
+      Glyphs.unequipAll(player.options.respecIntoProtected && Glyphs.findFreeIndex(true) === -1);
+      Glyphs.refreshActive();
     }
   }
+
+  player.celestials.pelle.galaxyGenerator.generatedGalaxies =
+    player.celestials.pelle.galaxyGenerator.generatedGalaxies.add(this.gainPerSecond.times(diff.div(1000)));
+  player.celestials.pelle.galaxyGenerator.generatedGalaxies = Decimal.min(
+    player.celestials.pelle.galaxyGenerator.generatedGalaxies,
+    this.generationCap
+  );
+
+  if (!this.capRift) {
+    if (player.exposes.lt(300)) {
+      PelleRifts.all.forEach(r =>
+        r.reducedTo = diff.div(1e5).mul(3).add(r.reducedTo)
+          .clampMax(Decimal.clampMax(player.exposes, 22).add(2))
+      );
+    } else if (player.exposes.lt(200)) {
+      PelleRifts.all.forEach(r =>
+        r.reducedTo = diff.div(1e5).mul(3).add(r.reducedTo)
+          .clampMax(Decimal.clampMax(player.exposes, 20).add(2))
+      );
+    } else if (player.exposes.lt(100)) {
+      PelleRifts.all.forEach(r =>
+        r.reducedTo = diff.div(1e5).mul(3).add(r.reducedTo)
+          .clampMax(Decimal.clampMax(player.exposes, 15).add(2))
+      );
+    } else {
+      PelleRifts.all.forEach(r =>
+        r.reducedTo = diff.div(1e5).mul(3).add(r.reducedTo)
+          .clampMax(Decimal.clampMax(player.exposes, 23).add(2))
+      );
+    }
+
+    if (PelleRifts.vacuum.milestones[0].canBeApplied && !this.hasReturnedGlyphSlot) {
+      Glyphs.refreshActive();
+      EventHub.dispatch(GAME_EVENT.GLYPHS_EQUIPPED_CHANGED);
+      this.hasReturnedGlyphSlot = true;
+    }
+  }
+}
 };
 
 export class GalaxyGeneratorUpgrade extends RebuyableMechanicState {
